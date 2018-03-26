3b23fefea3118567b34e5d7fcd7a3a822746a1d1
Niklas <niklaskapelle@gmail.com>
2018-03-26 15:23:46 +0200

    main changes

---
+++ src/SASBuildUp.java
@@ -0,0 +1,20 @@
+public class SASBuildUp {
+
+    private Fluglinie theAirline;
+    private Pilot thePilot;
+    private Stadt destCity;
+    private Stadt depCity;
+    private Flughafen destAirport;
+    private Flughafen depAirport;
+    private Passagier p1;
+    private Passagier p2;
+    private Flugzeug theAirplane;
+    private Flug outbound;
+    private Flug inbound;
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
