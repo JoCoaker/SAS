d99a59fe155f8ed380489a03b57795e2e1b86aa4
Luke <utilize.shop@gmail.com>
2018-03-26 15:04:35 +0200

    Added Passagier and Main

---
+++ src/Passagier.java
@@ -0,0 +1,39 @@
+
+/**
+ * Passagier
+ *
+ * @author Felix Rueß [199261]
+ * @version 1.0.0
+ */
+public class Passagier {
+    private String name;
+    private String vorname;
+
+    private String geschlecht;
+    private Flug reiseMittel;
+    private Sitzplatz sitzplatz;
+
+    public Passagier(String name, String vorname, String geschlecht){
+        this.name = name;
+        this.vorname = vorname;
+        this.geschlecht = geschlecht;
+
+        System.out.println("Passagier " + name + " " + vorname + " angelegt.");
+    }
+
+
+    public String getName(){ return name;}
+
+    public String getVorname() { return vorname;}
+
+    public String getGeschlecht() { return geschlecht;}
+
+    public Flug getReiseMittel() { return reiseMittel;}
+
+    public Sitzplatz getSitzplatz() { return sitzplatz;}
+
+    public void setSitzplatz(Sitzplatz sitzplatz){ this.sitzplatz = sitzplatz; }
+
+    public void setReisemittel(Flug reiseMittel){ this.reiseMittel = reiseMittel; }
+    public String toString(){ return this.getClass().getName() +  " " + this.name + " " + this.vorname;}
+}
